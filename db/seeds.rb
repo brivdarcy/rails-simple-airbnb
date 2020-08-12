eb8a4a6f19c407fe25a55ef5ef179a445775b061
ODBenedicte des Arcis <bdesarcis@gmail.com>
2020-08-12 18:04:07 +0200

    map

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+5.times do
+  Flat.create!(
+    name: Faker::Lorem.sentence(word_count: 5),
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    price_per_night: rand(75..150),
+    number_of_guests: rand(1..6)
+  )
+end
